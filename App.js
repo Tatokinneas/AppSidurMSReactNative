197f8d563f3863f9da23f65ae31f7bb25940150b
Octavio Benitez <tatokinneas@gmail.com>
2022-12-15 11:42:15 -0600

    Feat: Initial Project

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 
 export default function App() {
+  let logo =require("./assets/LogoMS.png");
+
   return (
     <View style={styles.container}>
+      <Image source={logo} style={styles.image}></Image>
+      <Pressable><Text>Hola</Text></Pressable>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -17,4 +21,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{
+    width: 300,
+    height: 300,
+    resizeMode:'contain',
+  }
+  
 });
