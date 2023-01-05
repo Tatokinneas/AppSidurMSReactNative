0783264881ec02a5faf1fea1c60e84df8f4801e2
Octavio Benitez <tatokinneas@gmail.com>
2023-01-05 16:36:59 -0600

    Feat: Api

--- Api/apiAshre.js
+++ Api/apiAshre.js
@@ -9,16 +9,16 @@ let Ashre8 = require("../assets/Photos/SIDUR-061.png");
 let Ashre9 = require("../assets/Photos/SIDUR-062.png");
 
 const ashreObject = [
-  { id: 1, image: Ashre },
-  { id: 2, image: Ashre1 },
-  { id: 3, image: Ashre2 },
-  { id: 4, image: Ashre3 },
-  { id: 5, image: Ashre4 },
-  { id: 6, image: Ashre5 },
-  { id: 7, image: Ashre6 },
-  { id: 8, image: Ashre7 },
-  { id: 9, image: Ashre8 },
-  { id: 10, image: Ashre9 },
+  { id: 1, image: Ashre1 },
+  { id: 2, image: Ashre2 },
+  { id: 3, image: Ashre3 },
+  { id: 4, image: Ashre4 },
+  { id: 5, image: Ashre5 },
+  { id: 6, image: Ashre6 },
+  { id: 7, image: Ashre7 },
+  { id: 8, image: Ashre8 },
+  { id: 9, image: Ashre9 },
+  // { id: 10, image: Ashre9 },
 ];
 
 export default ashreObject;
