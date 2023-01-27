98cf3eb25b079b122945c0fed144436eb4ef5261
EliasPenhosA <elias2penhos@gmail.com>
2023-01-27 08:31:46 -0600

    agradeciminto terminados

---
+++ Api/apiAgra.js
@@ -0,0 +1,16 @@
+let agra1 = require("../assets/Photos/DERECHOS RESERVADOS/agra1.png");
+let agra2 = require("../assets/Photos/DERECHOS RESERVADOS/agra2.png");
+let agra3 = require("../assets/Photos/DERECHOS RESERVADOS/agra3.png");
+let agra4 = require("../assets/Photos/DERECHOS RESERVADOS/agra4.png");
+
+
+const agraObject = [
+  { id: 1, image: agra1 },
+  { id: 2, image: agra2 },
+  { id: 3, image: agra3 },
+  { id: 4, image: agra4 },
+
+  
+];
+
+export default agraObject;
